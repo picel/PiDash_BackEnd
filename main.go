4fca7daa29950d08edcf5049efe5614f0978d16a
picel <picel@picel.net>
2023-12-09 15:55:26 +0900

    add systray function

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/getlantern/systray"
 	"github.com/gorilla/mux"
 	"picel.pidash/api"
 	"picel.pidash/ws"
 )
 
 func main() {
+	go func() {
+		systray.Run(onReady, nil)
+	}()
+
 	router := mux.NewRouter()
 
 	apiRouter := router.PathPrefix("/api").Subrouter()
@@ -19,4 +25,19 @@ func main() {
 	ws.SetUpRoutes(wsRouter)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func onReady() {
+	systray.SetTitle("PiDash")
+	systray.SetTooltip("PiDash")
+
+	mRunning := systray.AddMenuItem("PiDash Server Running", "")
+	mRunning.Disable()
+
+	mQuit := systray.AddMenuItem("Quit", "Quit PiDash")
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+		os.Exit(0)
+	}()
 }
